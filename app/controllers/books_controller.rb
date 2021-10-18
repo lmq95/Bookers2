d74ec5d573d0d1591a16e91d3e80434ed4056846
lmq95 <4321namap@gmail.com>
2021-10-18 11:10:20 +0000

    [update books]4-10]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,52 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      flash.now[:alert]
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
   end
 
-  def edit
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = "Book was successfully updated."
+       redirect_to book_path
+    else
+      flash.now[:alert]
+      render :edit
+    end
   end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
+  end
+  
+
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    @user = User.new
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:id])
   end
 
   def edit
   end
+  
+  
 end
