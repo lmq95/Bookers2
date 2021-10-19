8b785508dcc6fce3fba16c88f75b9d5050f1e04e
lmq95 <4321namap@gmail.com>
2021-10-19 18:35:52 +0000

    [add template]4-10]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+    @user = User.find(params[:id])
+    @books = @user.books
   end
-
-  def edit
     
+  def edit
+    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)  
   end
   
   
