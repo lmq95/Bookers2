b9c5bc980f70fee1aeb2367bcc8cab237a3b438c
lmq95 <4321namap@gmail.com>
2021-10-20 17:17:14 +0000

    [update]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @user = User.new
+    @books = Book.all
+    @book = Book.new
   end
-
+  
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -14,6 +16,9 @@ class UsersController < ApplicationController
   end
   
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
   
   
@@ -21,5 +26,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
-  
+
 end
