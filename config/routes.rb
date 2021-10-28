ba2ae86270a6ed99c4703fc2e724f5d258e2bf66
MaoTorihata <4321namap@gmail.com>
2021-10-28 05:46:32 +0000

    [add icon]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get '/home/about' => 'homes#about', as: 'about'
   devise_for :users
   post '/users/:id' => 'users#create'
-  resources :users, only: [:index, :create, :show, :edit, :update]
+  
+  resources :users, only: [:index, :create, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+  end  
+  
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
