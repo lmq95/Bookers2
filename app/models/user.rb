256474ff6ce1c075b744722718992e2e8d4944a2
MaoTorihata <4321namap@gmail.com>
2021-10-29 15:36:13 +0000

    [update]4-10

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
-  
-  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followings, through: :relationships, source: :followr
-  
+
+
 
    attachment :profile_image
 
