0748f89df56cd8d5e12ffd57c37aa623ca9f23cd
MaoTorihata <4321namap@gmail.com>
2021-11-01 16:49:31 +0000

    [finish follow]

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
 
 # ——————フォロー機能を作成・保存・削除する————————————
   def create
-    current_user.follow(params[:id])
+    current_user.follow(params[:user_id])
     redirect_to request.referer
   end
 
   def destroy
-    current_user.unfollow(params[:id])
+    current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end
 
